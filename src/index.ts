dbdd735e7a0e6e2811506402c15f2022c6791e85
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-25 15:47:08 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,20 @@ import stream from 'stream';
 
 const PLUGIN_NAME = 'gulp-chrome-web-store';
 
-// tslint:disable: no-console
-export = function Plugin(
-  credential: string | Credential,
-  accessTokenResponse: string | AccessTokenResponse,
-) {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export = function Plugin(credential: string | Credential, accessTokenResponse: string | AccessTokenResponse) {
   const chromeWebStore = new ChromeWebStore(
     typeof credential === 'string' ? JSON.parse(credential) : credential,
     typeof accessTokenResponse === 'string' ? JSON.parse(accessTokenResponse) : accessTokenResponse,
   );
   return new class {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     public item(id: string) {
       return {
-        upload(uploadType: UploadType = '') {
+        upload(uploadType: UploadType = ''): stream.Transform {
           return new stream.Transform({
             objectMode: true,
-            transform(vinyl, encoding, callback) {
+            transform(vinyl, encoding, callback): void {
               Promise.resolve(new chromeWebStore.Item(id)).then(async item => {
                 const result = await item.upload(vinyl.contents, uploadType);
                 if (result.uploadState === 'FAILURE') {
@@ -32,7 +30,7 @@ export = function Plugin(
             },
           });
         },
-        async publish(publishTarget: PublishTarget = 'default') {
+        async publish(publishTarget: PublishTarget = 'default'): Promise<void> {
           try {
             const item = new chromeWebStore.Item(id);
             const result = await item.publish(publishTarget);
